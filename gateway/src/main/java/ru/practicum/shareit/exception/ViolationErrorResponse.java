8d6f18819ef47e7e0b4785f7496e88de9f327165
Romanenko3490 <Saff-1@yandex.ru>
2025-09-18 21:28:20 +0300

    tests completed

--- gateway/src/main/java/ru/practicum/shareit/exception/ViolationErrorResponse.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/ViolationErrorResponse.java
@@ -9,5 +9,5 @@ import java.util.List;
 @Getter
 @RequiredArgsConstructor
 public class ViolationErrorResponse {
-    private final List<Violation> violations;
+    private final List<Violation> error;
 }
